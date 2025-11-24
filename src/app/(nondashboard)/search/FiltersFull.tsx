3db8688049f1fb447784bb970304dd1618d30eda
RogerGN <roger.goulart.nascimento@gmail.com>
2025-11-24 15:44:13 -0300

    @feat: create page search, filtersBar and filtersFull

---
+++ src/app/(nondashboard)/search/FiltersFull.tsx
@@ -0,0 +1,297 @@
+/** biome-ignore-all lint/a11y/useKeyWithClickEvents: <explanation> */
+/** biome-ignore-all lint/a11y/noStaticElementInteractions: <explanation> */
+
+import { debounce } from "lodash"
+import { Search } from "lucide-react"
+import { usePathname, useRouter } from "next/navigation"
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Slider } from "@/components/ui/slider"
+import { AmenityIcons, PropertyTypeIcons } from "@/lib/constants"
+import { cleanParams, cn, formatEnumString } from "@/lib/utils"
+import { type FiltersState, initialState, setFilters } from "@/state"
+import { useAppSelector } from "@/state/redux"
+
+const FiltersFull = () => {
+  const dispatch = useDispatch()
+  const pathname = usePathname()
+  const router = useRouter()
+  const { filters, isFiltersFullOpen } = useAppSelector((state) => state.global)
+  const [localFilters, setLocalFilters] = useState(initialState.filters)
+
+  //functions
+
+  const updateURL = debounce((newFilters: FiltersState) => {
+    const cleanFilters = cleanParams(newFilters)
+    const updateSearchParams = new URLSearchParams()
+
+    Object.entries(cleanFilters).forEach(([key, value]) => {
+      updateSearchParams.set(
+        key,
+        Array.isArray(value) ? value.join(",") : value.toString(),
+      )
+    })
+
+    router.push(`${pathname}?${updateSearchParams.toString()}`)
+  })
+
+  const handleSubmit = () => {
+    dispatch(setFilters(localFilters))
+    updateURL(localFilters)
+  }
+
+  const handleReset = () => {
+    setFilters(initialState.filters)
+    dispatch(setFilters(initialState.filters))
+    updateURL(initialState.filters)
+  }
+
+  const handleAmenityChange = (amenity: AmenityEnum) => {
+    setLocalFilters((prev) => ({
+      ...prev,
+      amenities: prev.amenities.includes(amenity)
+        ? prev.amenities.filter((a) => a !== amenity)
+        : [...prev.amenities, amenity],
+    }))
+  }
+
+  const handleLocationSearch = async () => {
+    try {
+      const response = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+          localFilters.location,
+        )}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}&fuzzyMatch=true`,
+      )
+      const data = await response.json()
+      if (data.features && data.features.length > 0) {
+        const [lng, lat] = data.features[0].center
+        setLocalFilters((prev) => ({ ...prev, coordinates: [lng, lat] }))
+      }
+    } catch (error) {
+      console.error("Error search loaction: ", error)
+    }
+  }
+
+  if (!isFiltersFullOpen) return null
+
+  return (
+    <div className="bg-white rounded-lg px-4 h-full overflow-x-hidden scrollbar-hide pb-10 scr">
+      <div className="flex flex-col space-y-6">
+        {/* Location */}
+        <div>
+          <h4 className="font-bold mb-2">Location</h4>
+          <div className="flex items-center">
+            <Input
+              placeholder="Enter Location"
+              value={filters.location}
+              onChange={(e) =>
+                setLocalFilters((prev) => ({
+                  ...prev,
+                  location: e.target.value,
+                }))
+              }
+              className="rounded-l-xl rounded-r-none border-r-0"
+            />
+            <Button
+              onClick={handleLocationSearch}
+              className="rounded-r-xl rounded-l-none border-black shadow-none border hover:bg-primary-700 hover:text-primary-50"
+            >
+              <Search className="w-4 h-4" />
+            </Button>
+          </div>
+        </div>
+
+        {/* Property Type */}
+        <div>
+          <h4 className="font-bold mb-2">Property Type</h4>
+          <div className="grid grid-cols-2 gap-4">
+            {Object.entries(PropertyTypeIcons).map(([type, Icon]) => (
+              <div
+                key={type}
+                className={cn(
+                  "flex flex-col items-center justify-center p-4 border rounded-xl cursor-pointer",
+                  localFilters.propertyType === type
+                    ? "border-black"
+                    : "border-gray-200",
+                )}
+                onClick={() =>
+                  setLocalFilters((prev) => ({
+                    ...prev,
+                    propertyType: type as PropertyTypeEnum,
+                  }))
+                }
+              >
+                <Icon className="w-6 h-6 mb-2" />
+                <span>{type}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* Price Range */}
+        <div>
+          <h4 className="font-bold mb-2">Price Range (Monthly)</h4>
+          <Slider
+            min={0}
+            max={10000}
+            step={100}
+            value={[
+              localFilters.priceRange[0] ?? 0,
+              localFilters.priceRange[1] ?? 10000,
+            ]}
+            onValueChange={(value: any) =>
+              setLocalFilters((prev) => ({
+                ...prev,
+                priceRange: value as [number, number],
+              }))
+            }
+          />
+          <div className="flex justify-between mt-2">
+            <span>${localFilters.priceRange[0] ?? 0}</span>
+            <span>${localFilters.priceRange[1] ?? 10000}</span>
+          </div>
+        </div>
+
+        {/* Beds and Baths */}
+        <div className="flex gap-4">
+          <div className="flex-1">
+            <h4 className="font-bold mb-1">Beds</h4>
+            <Select
+              value={localFilters.beds || "any"}
+              onValueChange={(value) =>
+                setLocalFilters((prev) => ({ ...prev, beds: value }))
+              }
+            >
+              <SelectTrigger className="w-full rounded-xl">
+                <SelectValue placeholder="Beds" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="any">Any Beds</SelectItem>
+                <SelectItem value="1">1+ bed</SelectItem>
+                <SelectItem value="2">2+ bed</SelectItem>
+                <SelectItem value="3">3+ bed</SelectItem>
+                <SelectItem value="4">4+ bed</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex-1">
+            <h4 className="font-bold mb-1 ml-1">Baths</h4>
+            <Select
+              value={localFilters.baths || "any"}
+              onValueChange={(value) =>
+                setLocalFilters((prev) => ({ ...prev, baths: value }))
+              }
+            >
+              <SelectTrigger className="w-full rounded-xl">
+                <SelectValue placeholder="Beds" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="any">Any baths</SelectItem>
+                <SelectItem value="1">1+ bath</SelectItem>
+                <SelectItem value="2">2+ baths</SelectItem>
+                <SelectItem value="3">3+ baths</SelectItem>
+                <SelectItem value="4">4+ baths</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+
+        {/* Square Feet */}
+        <div>
+          <h4 className="font-bold mb-2">Square Feet</h4>
+          <Slider
+            min={0}
+            max={5000}
+            step={100}
+            value={[
+              localFilters.squareFeet[0] ?? 0,
+              localFilters.squareFeet[1] ?? 5000,
+            ]}
+            onValueChange={(value) =>
+              setLocalFilters((prev) => ({
+                ...prev,
+                squareFeet: value as [number, number],
+              }))
+            }
+            className="[&>.bar]:bg-primary-700"
+          />
+          <div className="flex justify-between mt-2">
+            <span>{localFilters.squareFeet[0] ?? 0} sq ft</span>
+            <span>{localFilters.squareFeet[1] ?? 5000} sq ft</span>
+          </div>
+        </div>
+
+        {/* Amenities */}
+        <div>
+          <h4 className="font-bold mb-2">Amenities</h4>
+          <div className="flex flex-wrap gap-2">
+            {Object.entries(AmenityIcons).map(([amenity, Icon]) => (
+              <div
+                key={amenity}
+                className={cn(
+                  "flex items-center space-x-2 p-2 border rounded-lg hover:cursor-pointer",
+                  localFilters.amenities.includes(amenity as AmenityEnum)
+                    ? "border-black"
+                    : "border-gray-200",
+                )}
+                onClick={() => handleAmenityChange(amenity as AmenityEnum)}
+              >
+                <Icon className="w-5 h-5 hover:cursor-pointer" />
+                <Label className="hover:cursor-pointer">
+                  {formatEnumString(amenity)}
+                </Label>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* Available from */}
+        <div>
+          <h4 className="font-bold mb-2">Availble From</h4>
+          <Input
+            type="date"
+            value={
+              localFilters.availableFrom !== "any"
+                ? localFilters.availableFrom
+                : ""
+            }
+            onChange={(e) =>
+              setLocalFilters((prev) => ({
+                ...prev,
+                availableFrom: e.target.value ? e.target.value : "any",
+              }))
+            }
+            className="rounded-xl"
+          />
+        </div>
+
+        {/* Apply and reset buttons */}
+        <div className="flex gap-4  mt-6">
+          <Button
+            onClick={handleSubmit}
+            className="flex-1 bg-primary-700 text-white rounded-xl hover:bg-primary-700/80 hover:underline"
+          >
+            Apply
+          </Button>
+          <Button
+            onClick={handleReset}
+            variant="outline"
+            className="flex-1 rounded-xl"
+          >
+            Reset Filters
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FiltersFull
