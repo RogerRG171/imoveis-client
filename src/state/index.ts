d323270df7090b356314649a220ae941a4e36ff4
RogerGN <roger.goulart.nascimento@gmail.com>
2026-02-09 15:39:26 -0300

    @update: change mock data for state filter

--- src/state/index.ts
+++ src/state/index.ts
@@ -20,7 +20,7 @@ interface InitialStateTypes {
 
 export const initialState: InitialStateTypes = {
   filters: {
-    location: "Los Angeles",
+    location: "Torres",
     beds: "any",
     baths: "any",
     propertyType: "any",
@@ -28,7 +28,7 @@ export const initialState: InitialStateTypes = {
     availableFrom: "any",
     priceRange: [null, null],
     squareFeet: [null, null],
-    coordinates: [-118.25, 34.05],
+    coordinates: [-49.761184, -29.321007],
   },
   isFiltersFullOpen: false,
   viewMode: "grid",
