cbf8f27cc90f10565b6438e7d86a157dd48544ca
RogerGN <roger.goulart.nascimento@gmail.com>
2026-02-23 16:52:48 -0300

    @fix: new aws url

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "*.aws.amazon.com",
+        hostname: "*.amazonaws.com",
         port: "",
         pathname: "/**",
       },
